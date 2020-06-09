948f3504c62cc64024d6cc63200cd8f0e2b0d896
Emily Jones <emjones829@gmail.com>
2020-06-09 21:07:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,11 @@
 
 def sort_array_asc(array)
-  array = [25, 7, 1]
   array.sort
 end
 
-array = [25, 7, 14]
+
 def sort_array_des(array)
+  array = [25, 7, 14]
   array.sort! do |a, b|
     b <=> a
   end
