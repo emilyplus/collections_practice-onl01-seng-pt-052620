3bf3bdf1fe67135b6c798f30ab950c6fac12484f
Emily Jones <emjones829@gmail.com>
2020-06-09 21:20:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,3 +8,8 @@ def sort_array_desc(array)
   array.sort! { |a, b| b <=> a }
 end
 
+
+def sort_array_char_count(array)
+  array = ["foo", "test", "boink")
+  array.sort { |a,b| a.length <=> b.length }
+end
